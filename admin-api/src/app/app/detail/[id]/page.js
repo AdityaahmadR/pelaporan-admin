5ca0712d26490d8777697a3b31a25d3887ddaa06
Aditya Ahmad Reinaldy <nime7fix@gmail.com>
2025-12-04 16:07:13 +0800

    test104

--- admin-api/src/app/app/detail/[id]/page.js
+++ admin-api/src/app/app/detail/[id]/page.js
@@ -4,7 +4,6 @@ import { useEffect, useState } from 'react';
 import Sidebar from '@/components/Sidebar';
 import SearchBar from '@/components/SearchBar';
 import styles from './Detail.module.css';
-import styles from '@/app/darurat/darurat.module.css';  // PAKAI CSS YANG SAMA!
 
 export default function DetailPage({ params }) {
   const { id } = params;
