be4fcae1a1cda519662834a62e1e4f9fe0c51ac7
Aditya Ahmad Reinaldy <nime7fix@gmail.com>
2025-12-16 12:26:22 +0800

    Clean up duplicate files - remove src/, public/, node_modules/, and config files from root since they're now in admin-api/

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Next.js 16 - App Directory sudah stable
-  experimental: {
-    appDir: true,
-  },
-};
-
-export default nextConfig;
